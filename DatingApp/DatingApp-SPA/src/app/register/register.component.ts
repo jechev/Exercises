996cf4c7b32ca1f93b7d8906dada783997e0101f
Dimitar Zhechev <dimityr.jechev@gmail.com>
2019-05-09 17:28:46 +0300

    Added 3rd party components for notication, jwt token and bootstrap

--- DatingApp/DatingApp-SPA/src/app/register/register.component.ts
+++ DatingApp/DatingApp-SPA/src/app/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
+import { AlertifyService } from '../_services/alertify.service';
 
 @Component({
   selector: 'app-register',
@@ -11,7 +12,7 @@ export class RegisterComponent implements OnInit {
   @Output() cancelRegister = new EventEmitter();
   model: any = {};
 
-  constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService, private alertifyService: AlertifyService) { }
 
   ngOnInit() {
   }
@@ -19,16 +20,14 @@ export class RegisterComponent implements OnInit {
   register() {
     this.authService.register(this.model)
     .subscribe(() => {
-      console.log('Registration successful');
+      this.alertifyService.success('Registration successful');
     }, error => {
-      console.log(error);
+      this.alertifyService.error(error);
     });
-    console.log(this.model);
   }
 
   cancel() {
     this.cancelRegister.emit(false);
-    console.log('cancelled');
   }
 
 }
